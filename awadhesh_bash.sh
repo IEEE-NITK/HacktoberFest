d005649b3d84f055fc83166d6ff1948e8fea25f9
awadheshrawat <awadhesh31dec@outlook.com>
2016-10-27 15:46:50 +0530

    # Print message in center of terminal

---
+++ awadhesh_bash.sh
@@ -0,0 +1,16 @@
+
+    #!/bin/bash
+    cols=$( tput cols )
+    rows=$( tput lines )
+    message=$@
+    input_length=${#message}
+    half_input_length=$(( $input_length / 2 ))
+    middle_row=$(( $rows / 2 ))
+    middle_col=$(( ($cols / 2) - $half_input_length ))
+    tput clear
+    tput cup $middle_row $middle_col
+    tput bold
+    echo $@
+    tput sgr0
+    tput cup $( tput lines ) 0
+
