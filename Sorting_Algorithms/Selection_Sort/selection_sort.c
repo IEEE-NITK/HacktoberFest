863b217346d6cdaa0adb197a7030f16204834c43
Salman Shah <sbs.191197@gmail.com>
2016-11-01 07:46:03 +0530

    Repo re-formatted

--- Sorting_Algorithms/Selection_Sort/selection_sort.c
+++ Sorting_Algorithms/Selection_Sort/selection_sort.c
@@ -1,44 +1,38 @@
 #include <stdio.h>
- 
-void swap(int *x, int *y)
-{
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
- 
-void selectionSort(int arr[], int n)
-{
-    int i, j, min;
-    for (i = 0; i < n-1; i++)
-    {
-        min=i;
-        for (j = i+1; j < n; j++)
-          if (arr[j] < arr[min])
-            min = j;
-        swap(&arr[min], &arr[i]);
-    }
-}
 
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
- 
 int main()
 {
-    int arr[100];
-    int n,i;
-    printf("Enter number of Elements tobe sorted: \n");
-    scanf("%d",&n);
-    printf("Enter %d Numbers\n",n);
-    for(i=0;i<n;i++)
-	scanf("%d",&arr[i]);
-    selectionSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
+   int array[10010], n, c, d, position, swap;
+
+   printf("Enter number of elements\n");
+   scanf("%d", &n);
+
+   printf("Enter %d integers\n", n);
+
+   for ( c = 0 ; c < n ; c++ )
+      scanf("%d", &array[c]);
+
+   for ( c = 0 ; c < ( n - 1 ) ; c++ )
+   {
+      position = c;
+
+      for ( d = c + 1 ; d < n ; d++ )
+      {
+         if ( array[position] > array[d] )
+            position = d;
+      }
+      if ( position != c )
+      {
+         swap = array[c];
+         array[c] = array[position];
+         array[position] = swap;
+      }
+   }
+
+   printf("Sorted list in ascending order:\n");
+
+   for ( c = 0 ; c < n ; c++ )
+      printf("%d\n", array[c]);
+
+   return 0;
 }
