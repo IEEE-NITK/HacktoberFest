02b3cc25c13746ef9424f9778baef7be4d919252
Ankit Kataria <ankitkataria66@gmail.com>
2017-10-06 23:14:09 +0530

    Done many changes!
    1. Added '/' operator in printf
    2.Removed spaces between format specifiers in scanf
    3.Changed format specifiers from %.11f to %.2f so that it can take 2 decimal places and shows answer accurate

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -5,28 +5,28 @@ int main() {
     char op;
     double n1,n2;
 
-    printf("Enter an operator (+, -, *,): ");
-    scanf(" %c ", &op);
+    printf("Enter an operator (+, -, *,/): ");
+    scanf("%c",&op);
 
     printf("Enter two operands: ");
-    scanf("%lf %lf ",&n1, &n2);
+    scanf("%lf%lf",&n1,&n2);
 
     switch(op)
     {
         case '+':
-            printf("%.1lf + %.1lf = %.1lf\n",n1, n2, n1 + n2);
+            printf("%.2f + %.2f = %.2f\n",n1, n2, n1 + n2);
             break;
 
         case '-':
-            printf("%.1lf - %.1lf = %.1lf\n",n1, n2, n1 - n2);
+            printf("%.2f - %.2f = %.2f\n",n1, n2, n1 - n2);
             break;
 
         case '*':
-            printf("%.1lf * %.1lf = %.1lf\n",n1, n2, n1 * n2);
+            printf("%.2f * %.2f = %.2f\n",n1, n2, n1 * n2);
             break;
 
         case '/':
-            printf("%.1lf / %.1lf = %.1lf\n",n1, n2, n1 / n2);
+            printf("%.2f / %.2f = %.2f\n",n1, n2, n1 / n2);
             break;
 
         // operator doesn't match any case constant (+, -, *, /)
